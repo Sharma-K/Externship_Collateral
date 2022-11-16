fb627b060f2af972ce0e582ef423e3020ee09675
Keshav Sharma <keshav2743@gmail.com>
2022-11-17 02:01:26 +0530

    added components

---
+++ src/components/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Create = () =>{
+
+    return(
+        <div>
+            This is create component
+        </div>
+    )
+}
+
+export default Create;
---
+++ src/components/Functions.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Graphics from "./Graphics";
+import Ad from "./Ad";
+import Interactions from "./Interactions";
+import Promotions from "./Promotions";
+const Functions = (props) => {
+{console.log(props)};
+    return(
+        <div>
+        {props.show==='g' && <Graphics />}
+        {props.show==='a' && <Ad />}
+         {props.show==='i' && <Interactions />}   
+         {props.show==='p' && <Promotions/>}
+        
+        </div>
+    )
+
+}
+
+export default Functions;
